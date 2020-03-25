cc922ea17634c9398c56c9484305b1f5a5366b5a
Rafael <rafael@MacBook-Air-de-Rafael.local>
2020-03-25 17:09:28 -0300

    Created search bar component

---
+++ src/components/searchBar/index.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import arrowForward from '../../public/icons/arrowForward.svg'
+import search from '../../public/icons/search.svg'
+import './style.scss'
+
+
+class SearchBar extends Component {
+
+    constructor(props){
+        super(props)
+        this.state={
+            value:''
+        }
+    }
+    
+    onChange = (event)=>{
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    onKeyDown = (event)=>{
+        if (event.keyCode === 13) {
+            this.onSubmit()
+        }
+    }
+
+    onSubmit = () => {
+        console.log('submit')
+        if (this.props.onSubmit && this.state.value.length > 0) this.props.onSubmit();
+    }
+
+    submitButton = () => {
+        if (this.state.value.length >= 1){
+            return <img src={arrowForward} onClick={this.onSubmit} className='search-bar--submit-button'/>
+        }
+    }
+
+    render(){
+        return (
+            <div className='search-bar'>
+                <img src={search} className='search-bar--icon'/>
+                <input value={this.state.value} onChange={this.onChange} onKeyDown={this.onKeyDown}  type='text' placeholder={this.props.placeholder}></input>
+                {this.submitButton()}
+                <div className='underline'/>
+            </div>
+        )
+    }
+
+}
+
+
+export default SearchBar
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import TopText from '../../components/topText/'
 import Button from '../../components/button/'
+import SearchBar from '../../components/searchBar/'
 import './style.scss'
 
 class Home extends Component {
     render(){
         return <div id='home'>
-            <TopText value='Olá Vanusa'/>
-            <Button text='Hello' type='add'/>
+            <TopText value='Hi Vanusa!'/>
+            <Button text='Create new order' type='add'/>
+            <SearchBar placeholder='Search for customer here'/>
         </div>
     }
 }
